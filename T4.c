11b159575bbe4e933e8a3f88e717d48dbaae58fb
Gustavo-de-Oliveira <gustavo_de_oliveira@usp.br>
2018-06-17 19:42:45 -0300

    Versão 0.4

--- T4.c
+++ T4.c
@@ -2,10 +2,17 @@
 #include <stdlib.h>
 #include <math.h>
 
+/*
+TODO:
+FREE EM MATRIZ
+*/
+
 typedef struct fracao{
 	int numerador;
 	int denominador;
 } fracao;
+
+void Swap(fracao **matriz, int nEquacoes);
 
 int main(int argc, char const *argv[]){
 	int nEquacoes = 0, nCoeficientes = 0, qntdEquacoes = 0;
@@ -41,5 +48,26 @@ int main(int argc, char const *argv[]){
 		}
 	} while (qntdEquacoes < nEquacoes);
 
+	if (matriz[0][0].numerador == 0){
+		Swap(matriz, nEquacoes);
+	}
+
 	return 0;
 }
+
+void Swap(fracao **matriz, int nEquacoes){
+	for (int i = 0; i < nEquacoes; ++i){
+			if (matriz[i][0].numerador != 0){
+				//switch(matriz[0] e matriz[i])
+			}
+		}	
+}
+
+/*
+3 4
+r
+0 1 2 1 0 1 0 1
+2 1 0 1 0 1 0 1
+p
+3 1 -1 1 0 1 3 1
+*/
