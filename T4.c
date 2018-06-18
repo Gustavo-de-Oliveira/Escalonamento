c9623e49bbc11e7a7e6c74339a08c3a991679268
Gustavo-de-Oliveira <gustavo_de_oliveira@usp.br>
2018-06-18 20:28:42 -0300

    Versão 0.5

--- T4.c
+++ T4.c
@@ -12,7 +12,7 @@ typedef struct fracao{
 	int denominador;
 } fracao;
 
-void Swap(fracao **matriz, int nEquacoes);
+void Swap(fracao **matriz, int nEquacoes, int nCoeficientes);
 
 int main(int argc, char const *argv[]){
 	int nEquacoes = 0, nCoeficientes = 0, qntdEquacoes = 0;
@@ -49,16 +49,38 @@ int main(int argc, char const *argv[]){
 	} while (qntdEquacoes < nEquacoes);
 
 	if (matriz[0][0].numerador == 0){
-		Swap(matriz, nEquacoes);
+		Swap(matriz, nEquacoes, nCoeficientes);
+	}
+
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			printf("%d ", matriz[i][j].numerador/matriz[i][j].denominador);
+		}
+		printf("\n");
 	}
 
 	return 0;
 }
 
-void Swap(fracao **matriz, int nEquacoes){
+void Swap(fracao **matriz, int nEquacoes, int nCoeficientes){
+	int totalNumerador = 0, totalDenominador = 0;
 	for (int i = 0; i < nEquacoes; ++i){
 			if (matriz[i][0].numerador != 0){
-				//switch(matriz[0] e matriz[i])
+				for (int j = 0; j < nCoeficientes; ++j){
+					totalNumerador = matriz[i][j].numerador;
+					totalDenominador = matriz[i][j].denominador;
+
+					matriz[i][j].numerador = matriz[0][j].numerador;
+					matriz[i][j].denominador = matriz[0][j].denominador;
+
+					matriz[0][j].numerador = totalNumerador;
+					matriz[0][j].denominador = totalDenominador;
+
+					totalNumerador = 0; totalDenominador = 0;
+				}
+				break;
 			}
 		}	
 }
